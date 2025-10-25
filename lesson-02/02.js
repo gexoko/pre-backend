391866f1c59c7f6999e1f7b4ed5a140b20c34825
Roman_kh <gexoko@gmail.com>
2025-10-25 11:46:05 +0300

    02js-random+1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random()* 7)// your code
-let dice2 = Math.floor(Math.random()* 7)// your code
+let dice1 = Math.floor(Math.random()* 6 + 1)// your code
+let dice2 = Math.floor(Math.random()* 6 + 1)// your code
 let isWinningDouble // your code
 
 if (dice1 === dice2 && dice1 > 3) {
