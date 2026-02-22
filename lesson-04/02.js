b6f42d740f0bcd91a8229b8c60d5439021133b46
Roman_kh <gexoko@gmail.com>
2026-02-23 01:18:45 +0300

    l04-02js-1com

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,21 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        isDuplicate = false;
+        for (let j = 0; j < result.length; j++) {
+            if (result[j] === array[i]) {
+                isDuplicate = true;
+            }
+        }
+        if (!isDuplicate) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4]))
