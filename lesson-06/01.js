27691742ea9e83716aace0f99e945a0fd41e7661
Roman_kh <gexoko@gmail.com>
2026-04-07 18:42:23 +0300

    ls06-01-01cm

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,13 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = (array, callback) => {
+    let result= [];
+    for (let i = 0; i < array.length; i++) {
+        const shouldKeep = callback(array[i], i);
+        if (shouldKeep === true) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
